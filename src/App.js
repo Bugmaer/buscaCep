012e277203f2567996a134243e99c47a12428d8d
César Bugmaer <162216315+Bugmaer@users.noreply.github.com>
2024-09-13 20:13:15 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { FiSearch } from 'react-icons/fi'
+import './styles.css'
+
 function App() {
   return (
-    <div className="App">
-      <h1>test bom dia</h1>
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+
+      <div className="cointainerInput">
+        <input 
+        type="text"
+        placeholder="Digite seu CEP">
+        </input>
+      <button className="buttonSearch">
+        <FiSearch size={25} color="FFF"/>
+      </button>
+      
+      <main className='main'>
+        <h2>CEP: 79003222</h2>
+
+        <span>Rua Teste Algum</span>
+        <span>Complemento: Algum complemento</span>
+        <span>Vila Rosa</span>
+        <span>Viamão - RS</span>
+      </main>
+
+      </div>
+      
     </div>
   );
 }
