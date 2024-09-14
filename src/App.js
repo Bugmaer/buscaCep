b7579504a7aff0b65b5005a9b0d42f99a21cd6be
César Bugmaer <162216315+Bugmaer@users.noreply.github.com>
2024-09-13 22:12:50 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-import { FiSearch } from 'react-icons/fi'
-import './styles.css'
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
 import { useState } from 'react';
-import api from "./services/api"
-
+import api from "./services/api";
 
 function App() {
 
-  const [input, setInput] = useState('')
-  const [cep, setCep] = useState({})
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
 
-  async function handleSearch(){
-   // 94475740/json/
-
-    if(input === ""){
-      alert("Preencha com algum CEP")
+  async function handleSearch() {
+    if (input === "") {
+      alert("Preencha com algum CEP");
       return;
     }
 
     try {
-      const response = await api.get(`${input}/json/`)
-      setCep(response.data)
-      setInput("")
-    }catch{
-      alert("Ops, erro ao buscar!!")
-      setInput("")
-      
+      const response = await api.get(`${input}/json/`);
+      setCep(response.data);
+      setInput("");
+    } catch {
+      alert("Ops, erro ao buscar!!");
+      setInput("");
     }
-
   }
 
   return (
@@ -35,27 +30,27 @@ function App() {
 
       <div className="containerInput">
         <input 
-        type="text"
-        placeholder="Digite seu CEP"
-        value={input}
-        onChange={(event) => setInput(event.target.value)}>
-        </input>
-      <button className="buttonSearch" onClick={handleSearch} >
-        <FiSearch size={25} color="FFF"/>
-      </button>
+          type="text"
+          placeholder="Digite seu CEP"
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#FFF"/>
+        </button>
       </div>
       
-      <main className='main'>
-        <h2>CEP: 79003222</h2>
+      {Object.keys(cep).length > 0 && (
+        <main className='main'>
+          <h2>CEP: {cep.cep}</h2>
 
-        <span>Rua Teste Algum</span>
-        <span>Complemento: Algum complemento</span>
-        <span>Vila Rosa</span>
-        <span>Viamão - RS</span>
-      </main>
-      
+          <span>{cep.logradouro}.</span>
+          <span>Complemento: {cep.complemento ? cep.complemento : "Não possui."}</span>
+          <span>{cep.bairro}.</span>
+          <span>{cep.localidade} - {cep.uf}.</span>
+        </main>
+      )}
     </div>
-    
   );
 }
 
